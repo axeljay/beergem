0582a945450e0096d4641500f8d1ef37bf66927f
samboey <samuelkjboey@gmail.com>
2019-08-20 14:20:37 +0800

    Updated controller,model and table for guides

---
+++ app/controllers/guides_controller.rb
@@ -0,0 +1,48 @@
+class GuidesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @guides = Guide.all
+  end
+
+  def show
+    @guide = Guide.find(params[:id])
+  end
+
+  def new
+    @guides = Guide.all
+  end
+
+  end
+
+
+  def edit
+    @guide = Guide.find(params[:id])
+  end
+
+  def create
+    @guide = Guide.new(guide_params)
+    @guide.user = current_user
+    @guide.save
+
+    redirect_to guide_path(@guide)
+  end
+
+  def update
+    @guide = Guide.find(params[:id])
+    @guide.update(guide_params)
+    @guide.save
+    redirect_to guide_path(@guide)
+  end
+
+  def destroy
+    @guide = Guide.find(params[:id])
+    @guide.destroy
+    redirect_to guides_path
+  end
+
+    private
+    def guide_params
+      params.require(:guide).permit(:title, :description, :image)
+    end
+end
