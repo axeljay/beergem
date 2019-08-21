715194bc8e52165d39fe2fcd7361f62da4ec62c2
axejacob <email@axeljacob.com>
2019-08-21 09:57:45 +0800

    page routes included index(home)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'beers#index'
+  # root 'beers#index'
 
+  root :to => "home#index"
+
+
+  resources :home
   resources :beers
   resources :users
   resources :wishlist
