d258e18f220f7ada38fbe553cec07a9325efe0f0
axejacob <email@axeljacob.com>
2019-08-21 10:38:54 +0800

    clean up the Login button. It's on every page no but eventually, once use logs in, it should only appear on the index page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => [ :show, :index ]
 
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
 
   root :to => "home#index"
 
-
   resources :home
   resources :beers
   resources :users
