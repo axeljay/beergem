c4c58a44eb5cbfb6431f34296b36d92537d484c5
axejacob <email@axeljacob.com>
2019-08-25 21:30:14 +0800

    reset selection button

---
+++ app/controllers/ipa_controller.rb
@@ -0,0 +1,13 @@
+class IpaController < ApplicationController
+
+  before_action :authenticate_user!, :except => [ :show, :index ]
+
+  def index
+  end
+
+  def show
+    @ipa = Ipa.find(params[:id])
+
+  end
+
+end
