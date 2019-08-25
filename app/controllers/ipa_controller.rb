c2943837195c1cc754d1e37e63d813a1d61cdcb8
axejacob <email@axeljacob.com>
2019-08-25 23:22:33 +0800

    Same as before

--- app/controllers/ipa_controller.rb
+++ app/controllers/ipa_controller.rb
@@ -3,7 +3,13 @@ class IpaController < ApplicationController
   before_action :authenticate_user!, :except => [ :show, :index ]
 
   def index
+    @beers = Beer.all
   end
+
+  def show_image
+    @beers = Beer.find(params[:id])
+    send_data @beers.image, :type => 'image/png',:disposition => 'inline'
+end
 
   # def show
   #   @ipa = Ipa.find(params[:id])
